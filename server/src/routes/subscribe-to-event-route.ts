672e0cc7cb2f1f289a7393e7e5ea7a304357513d
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-02-19 20:33:01 +0000

    adds event subscription feature with API, DB schema, and logic

--- server/src/routes/subscribe-to-event-route.ts
+++ server/src/routes/subscribe-to-event-route.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { subscribeToEvent } from '../functions/subscribe-to-events'
 
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
@@ -13,17 +14,20 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
             }),
             response: {
                 201: z.object({
-                    name: z.string(),
-                    email: z.string()
+                    subscribeId: z.string(),
                 })
             },
         },
     }, async (request, reply) => {
         const { name, email } = request.body
+
+        const { subscribeId } = await subscribeToEvent({ 
+            name, 
+            email 
+        } )
     
         return reply.status(201).send({
-            name,
-            email
+            subscribeId
         })
     })
 }
